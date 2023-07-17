59321f11a94cf68bc1202bef053f13315950d07b
iammilo2016 <alanmiloto@gmail.com>
2023-07-16 18:35:44 -0600

    git -commit -m "added inputs.py"

--- Ass3 - Skeleton/inputs.py
+++ Ass3 - Skeleton/inputs.py
@@ -4,6 +4,11 @@ from report import *
 import math
 
 def main():
+    print('The Painter\'s Company Estimating Tool')
+    getPaintPrice()
+    getNumberOfCoats()
+    getNumberRooms()
+    
     '''
     Controls flow of the program. Gets the initial inputs, 
     iterates for the specified number of rooms, 
@@ -15,14 +20,23 @@ def main():
     pass
 
 def getPaintPrice():
+    enterpriceofPaint = float(input("Enter price of paint: "))
+    
+    return enterpriceofPaint
     '''
         Asks the user to enter the paint price and returns the value entered
 
         Returns:  paint price / gallon
     '''
-    pass
+
 
 def getNumberOfCoats():
+
+    enterCoats = int(input("Enter number of coats of paint to apply (1-4): "))
+    while enterCoats < 1 or enterCoats > 4:
+        print("Error: Number of coats must be between 1 and 4")
+        enterCoats = int(input("Enter number of coats of paint to apply (1-4): "))
+    return enterCoats
     ''' 
         Asks the user to enter the number of coats of paint 
         to apply (1-4) and returns value entered. Error checks the
@@ -30,9 +44,10 @@ def getNumberOfCoats():
 
         Returns:  number of coats of paint
     '''
-    pass
 
 def getNumberRooms():
+    enterRooms = int(input("How many rooms do you want to paint? "))
+    return enterRooms
     '''  
        Asks the user to enter the number of rooms 
        to paint and returns value entered
@@ -42,6 +57,13 @@ def getNumberRooms():
     pass
 
 def roomsMenu(nbrOfRooms):
+    roomMenuDict = {'1':'Rectangular','2':'Square','3':'Custom'}
+    print(roomMenuDict(1))
+    enterType = int(input("Enter type of room (1-3): "))
+    while enterType < 1 or enterType > 3:
+        print("Error: Type of room must be between 1 and 3")
+        enterType = int(input("Enter type of room (1-3): "))
+    return enterType
     '''
        Reads the shape of the room based on a list of valid shapes.
        Based on a roomMenuDict of 1:Rec, 2:Square and 3:Custom.
@@ -52,7 +74,7 @@ def roomsMenu(nbrOfRooms):
        Returns:  menu option selected (1,2,3)
     '''
     roomMenuDict = {'1':'Rectangular','2':'Square','3':'Custom'}
-    pass
+    
 
 def getNumberWindowsDoors():
     '''
